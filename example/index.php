e457fba54e6218f6076faf611af6bbf8f5879445
bim-g <ibmussafb@gmail.com>
2023-04-05 15:00:10 +0200

    [ENH] Add middleware support to groupe routing

--- example/index.php
+++ example/index.php
@@ -1,6 +1,6 @@
 <?php
 use Wepesi\Controller\UserController;
-use Wepesi\Middlleware\UserValidation;
+use Wepesi\Middleware\UserValidation;
 use Wepesi\Routing\Router;
 
 $router = new Router();
@@ -15,18 +15,22 @@ $router->get('/home',function (){
  */
 $router->group('/users', function () use ($router) {
     $router->get('/', [userController::class,'get_users']);
-    $router->get('/:id', [userController::class, 'get_user_detail'])
-        ->middleware([userValidation::class,'detail_user']);
-
-    $router->group('/admin', function () use ($router) {
-        $router->get('/delete/:id', 'Wepesi\Controller\UserController#delete_user')
-            ->middleware([userValidation::class,'detail_user']);
+    $router->group([
+        'pattern'=>'/group',
+        'middleware' => [userValidation::class,'detail_user']
+    ],function () use($router){
+        $router->get('/:id/detail', [userController::class, 'get_user_detail'])
+            ->middleware([userController::class, 'userExist']);
+        $router->get('/:id/delete', 'Wepesi\Controller\UserController#delete_user');
     });
 });
-
+/**
+ *  set custom 404 output
+ */
 $router->set404('**',function(){
     header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
     print('Not Found : ' . http_response_code());
     exit;
 });
+//
 $router->run();
