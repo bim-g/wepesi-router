26621f77fafeed8b0c64ac0b4f37b43c4b779d2f
bim-g <ibmussafb@gmail.com>
2025-05-14 14:32:07 +0200

    [REF] move example to file one namespace example.

--- example/UserValidation.php
+++ example/UserValidation.php
@@ -3,18 +3,20 @@
  * Copyright (c) 2023. Wepesi inc.
  */
 
-namespace Wepesi\Middleware;
+namespace Wepesi\Exemple;
 
 class UserValidation
 {
-    function detail_user($id){
-        if(!filter_var($id,FILTER_VALIDATE_INT)){
+    function detail_user($id)
+    {
+        if (!filter_var($id, FILTER_VALIDATE_INT)) {
             echo "you should provide an integer";
             exit;
         }
     }
 
-    function validateId($id){
+    function validateId($id)
+    {
         if (!filter_var($id, FILTER_VALIDATE_INT)) {
             echo 'your id should be an integer';
             exit;
